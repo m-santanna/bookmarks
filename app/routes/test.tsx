1023a88d03fe922f42c0f153bec999ee98e8411c
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2025-04-24 01:26:46 +0100

    switched to tanstack, added shadcn and tailwind

--- app/routes/test.tsx
+++
@@ -1,7 +0,0 @@
-export default function Test() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">Test</h1>
-    </div>
-  );
-}
