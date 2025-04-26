3bb2701db170eb2c78f74bca54d7307c2ce51a78
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2025-04-26 16:35:50 +0100

    fix project title and git repo

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -22,7 +22,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Bookmarks',
+        title: 'TidyTabs',
       },
     ],
     links: [
